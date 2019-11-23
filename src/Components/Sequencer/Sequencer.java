1980f721c566a3c04790961673af82c2ef772659
Morteza Ahmadi <morinob93@gmail.com>
2019-11-23 15:37:43 -0500

    send customPacket to all the replicas

--- src/Components/Sequencer/Sequencer.java
+++ src/Components/Sequencer/Sequencer.java
@@ -2,8 +2,10 @@ package Components.Sequencer;
 
 import Components.Component;
 import Config.ComponentConfig;
+import Config.SystemConfig;
 import Model.Network.Request;
 import Networking.CustomPacket;
+import Networking.CustomPacketType;
 import Networking.ReliablePacketHandler;
 
 import java.util.ArrayList;
@@ -11,7 +13,7 @@ import java.util.ArrayList;
 public class Sequencer extends Component {
     private int counter;
     private ReliablePacketHandler packetHandler;
-
+    private ArrayList<Request> allRequestsReceived;
     public void main(String[] args) {
         packetHandler = new ReliablePacketHandler(this);
     }
@@ -28,6 +30,16 @@ public class Sequencer extends Component {
         request.setLabel(++counter);
     }
 
+    private void sendToAllReplicas(CustomPacket customPacket) {
+        Request request = customPacket.getRequest();
+        labelizeRequest(request);
+        allRequestsReceived.add(request);
+        CustomPacket newCustomPacket = new CustomPacket(getConfig(), generatePacketID(), CustomPacketType.REQUEST, request);
+
+        packetHandler.sendPacket(newCustomPacket, SystemConfig.Bowser);
+        packetHandler.sendPacket(newCustomPacket, SystemConfig.Kirby);
+        packetHandler.sendPacket(newCustomPacket, SystemConfig.Richter);
+    }
 
     @Override
     public void handleCustomPacket(CustomPacket customPacket) {
@@ -36,7 +48,7 @@ public class Sequencer extends Component {
                 //ReliableCustomPacket.send(CustomPacket)
                 //TODO: send want packet
             case REQUEST:
-                //ReliableCustomPacket.send(Request)
+                sendToAllReplicas(customPacket);
                 //TODO: send request object
         }
     }
